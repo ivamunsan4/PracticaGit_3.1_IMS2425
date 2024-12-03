f9be8568e31ac6c84d1076ba21b54b6da43d34c3
IVÁN MUÑOZ SÁNCHEZ 1 <ivamunsan4@alu.edu.gva.es>
2024-12-03 15:16:25 +0100

    Formulario Inicial - IMS2425

--- ejercicioTelegramaGit_IMS2425/Form1.Designer.cs
+++ ejercicioTelegramaGit_IMS2425/Form1.Designer.cs
@@ -28,13 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTelegrama = new System.Windows.Forms.TextBox();
+            this.chkUrgente = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Texto";
+            // 
+            // txtTelegrama
+            // 
+            this.txtTelegrama.Location = new System.Drawing.Point(65, 73);
+            this.txtTelegrama.Multiline = true;
+            this.txtTelegrama.Name = "txtTelegrama";
+            this.txtTelegrama.Size = new System.Drawing.Size(475, 143);
+            this.txtTelegrama.TabIndex = 1;
+            // 
+            // chkUrgente
+            // 
+            this.chkUrgente.AutoSize = true;
+            this.chkUrgente.Location = new System.Drawing.Point(66, 238);
+            this.chkUrgente.Name = "chkUrgente";
+            this.chkUrgente.Size = new System.Drawing.Size(70, 17);
+            this.chkUrgente.TabIndex = 2;
+            this.chkUrgente.Text = "Urgente?";
+            this.chkUrgente.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Coste:";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(118, 265);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(127, 20);
+            this.txtPrecio.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(390, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 45);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(577, 333);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chkUrgente);
+            this.Controls.Add(this.txtTelegrama);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Control Versiones Git IMS2425";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTelegrama;
+        private System.Windows.Forms.CheckBox chkUrgente;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ejercicioTelegramaGit_IMS2425/Form1.cs
+++ ejercicioTelegramaGit_IMS2425/Form1.cs
@@ -16,5 +16,40 @@ namespace ejercicioTelegramaGit_IMS2425
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textoTelegrama;
+            char tipoTelegrama = 'o';
+            int numPalabras = 0;
+            double coste;
+            //Leo el telegrama
+            textoTelegrama = txtTelegrama.Text;
+            // telegrama urgente?
+            if (chkUrgente.Checked)
+                tipoTelegrama = 'u';
+            //Obtengo el número de palabras que forma el telegrama
+            //Primero defino los delimitadores de palabras
+            char[] delimitadores = new char[] { ' ', '\r', '\n' };
+            //Cuento el número de palabras.
+            numPalabras = textoTelegrama.Split(delimitadores, StringSplitOptions.RemoveEmptyEntries).Length;
+            //numPalabras = textoTelegrama.Length;
+            //Si el telegrama es ordinario
+            if (tipoTelegrama == 'o') //IMS_2324
+                if (numPalabras <= 10)
+                    coste = 2.5;
+                else
+                    coste = 0.5 * numPalabras;
+            else
+            //Si el telegrama es urgente
+            if (tipoTelegrama == 'u')
+                if (numPalabras <= 10)
+                    coste = 5;
+                else
+                    coste = 5 + 0.75 * (numPalabras - 10);
+            else
+                coste = 0;
+            txtPrecio.Text = coste.ToString() + " euros";
+        }
     }
 }
